7d3d10ee8806d5903cb3a2c73880624ba0373ab5
NH1Tkendo <ngobatai1711@gmail.com>
2025-08-13 19:53:53 +0700

    Update lab 4
    Signed-off-by: NH1Tkendo <ngobatai1711@gmail.com>

---
+++ 2212453_NgoBaTai_Lab4/BlogMvc/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BlogMvc
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "HelloWord",
+                url: "HelloWord/Welcome/{name}/{numTimes}",
+                defaults: new { controller = "HelloWord", action = "Welcome", name = UrlParameter.Optional, numTimes = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}

5d68a6aaa79c59c4a61ccc88b6b7035dcc2b43e6
NH1Tkendo <ngobatai1711@gmail.com>
2025-08-16 15:22:31 +0700

    Update lab 8
    Signed-off-by: NH1Tkendo <ngobatai1711@gmail.com>

---
+++ 2212453_NgoBaTai_Lab8/MVCProject/Controllers/PathsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class PathsController : Controller
+    {
+        private MVCDatabaseEntities db = new MVCDatabaseEntities();
+
+        // GET: Paths
+        public ActionResult Index()
+        {
+            List<Path> pathList = db.Paths.ToList();
+            foreach (Path path in pathList)
+            {
+                List<Path> listSubPath = (from p in db.Paths
+                                          where p.ParentID == path.PathID
+                                         && p.Display == 1 && p.Active == 1
+                                          orderby p.OrderNo ascending
+                                          select p).ToList(); 
+            }
+            return View(pathList);
+        }
+
+        // GET: Paths/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Path path = db.Paths.Find(id);
+            if (path == null)
+            {
+                return HttpNotFound();
+            }
+            return View(path);
+        }
+
+        // GET: Paths/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Paths/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PathID,PathName,PathDescription,PathURL,ImageURL,OrderNo,Display,ParentID,Active")] Path path)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Paths.Add(path);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(path);
+        }
+
+        // GET: Paths/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Path path = db.Paths.Find(id);
+            if (path == null)
+            {
+                return HttpNotFound();
+            }
+            return View(path);
+        }
+
+        // POST: Paths/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PathID,PathName,PathDescription,PathURL,ImageURL,OrderNo,Display,ParentID,Active")] Path path)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(path).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(path);
+        }
+
+        // GET: Paths/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Path path = db.Paths.Find(id);
+            if (path == null)
+            {
+                return HttpNotFound();
+            }
+            return View(path);
+        }
+
+        // POST: Paths/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Path path = db.Paths.Find(id);
+            db.Paths.Remove(path);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
